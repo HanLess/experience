d1517f28f6db278175cd3c351a66fc254ceaf9a7
韩宏轩 <hanhongxuan@hanhonguandeMBP.lan>
2018-05-29 00:36:58 +0800

    0529

--- java学习/secKill/src/main/java/org/seckill/dao/cache/RedisDao.java
+++ java学习/secKill/src/main/java/org/seckill/dao/cache/RedisDao.java
@@ -1,25 +1,44 @@
 package org.seckill.dao.cache;
 
+import com.dyuproject.protostuff.LinkedBuffer;
+import com.dyuproject.protostuff.ProtostuffIOUtil;
+import com.dyuproject.protostuff.runtime.RuntimeSchema;
 import org.seckill.entry.Seckill;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
 /**
  * Created by daojia on 2018-5-28.
  */
+@Component
 public class RedisDao {
     private final JedisPool jedisPool;
 
-    public RedisDao(String ip,int port){
-        jedisPool = new JedisPool(ip,port);
+    private RuntimeSchema<Seckill> schema = RuntimeSchema.createFrom(Seckill.class);
+
+    public RedisDao(){
+        jedisPool = new JedisPool("localhost",6379);
     }
 
+//    反序列化
     public Seckill getSeckill(long seckillId){
         try{
             Jedis jedis = jedisPool.getResource();
             try {
-                String key = "seckill:" + seckillId;
-                
+                String key = "seckillId:" + seckillId;
+                byte[] keyByets = key.getBytes();
+//                内部没有序列化，返回二进制码 -> 反序列化拿到需要的内容 -> Object
+//                自定义序列化
+                byte[] bytes = jedis.get(keyByets);
+                if(bytes != null){
+//                    空对象
+                    Seckill seckill = schema.newMessage();
+                    ProtostuffIOUtil.mergeFrom(bytes,seckill,schema);
+//                    被反序列化
+                    return seckill;
+                }
             }finally {
                 jedis.close();
             }
@@ -29,7 +48,26 @@ public class RedisDao {
         return null;
     }
 
+//    序列化
     public String putSeckill(Seckill seckill){
+        try{
+            Jedis jedis = jedisPool.getResource();
+            try{
+                long seckillId = seckill.getSeckillId();
+
+                String key = "seckillId:" + seckillId;
+                byte[] bytes = ProtostuffIOUtil.toByteArray(seckill,schema, LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE));
+
+                int timeout = 3600;
+                String result = jedis.setex(key.getBytes(),timeout,bytes);
+
+                return result;
+            }finally {
+                jedis.close();
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         return null;
     }
---
+++ java学习/secKill/src/test/java/org/seckill/dao/cache/RedisDaoTest.java
@@ -0,0 +1,53 @@
+package org.seckill.dao.cache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.seckill.entry.Seckill;
+import org.seckill.service.SeckillService;
+import org.seckill.service.impl.SeckillServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml",
+        "classpath:spring/spring-service.xml"
+})
+public class RedisDaoTest {
+    @Autowired
+    private RedisDao redisDao;
+    @Autowired
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckill() {
+        long seckillId = 1001;
+//        Seckill seckill = redisDao.getSeckill(seckillId);
+//        System.out.println(seckill);
+
+    }
+
+    @Test
+    public void putSeckill() {
+        Seckill seckill = seckillService.getById(1001);
+        String result = redisDao.putSeckill(seckill);
+        System.out.println(result);
+    }
+
+    @Test
+    public void testSeckill(){
+        long seckillId = 1001;
+        Seckill seckill = redisDao.getSeckill(seckillId);
+        if(seckill == null){
+            Seckill sseckill = seckillService.getById(1001);
+            String result = redisDao.putSeckill(sseckill);
+//            if(result.equals("ok")){
+                System.out.println("----------------");
+                System.out.println(redisDao.getSeckill(seckillId));
+//            }
+        }else{
+            System.out.println("++++++++++++++++");
+            System.out.println(seckill);
+        }
+    }
+}
