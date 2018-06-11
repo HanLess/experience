9395d3b4b7043727f7e9f65cdff675afc26ad404
HanLess <happydroll@163.com>
2018-06-12 00:59:46 +0800

    0612

---
+++ java学习/shop/shop/src/main/java/com/mmall/service/impl/userServiceImpl.java
@@ -0,0 +1,10 @@
+package com.mmall.service.impl;
+
+import com.mmall.service.IUserService;
+
+public class userServiceImpl implements IUserService {
+    @Override
+    public Object login(String username, String password) {
+        return null;
+    }
+}
