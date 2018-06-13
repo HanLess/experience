e49fed8986469b900621467422e3e2d3b1d7d443
HanLess <happydroll@163.com>
2018-06-13 23:17:57 +0800

    0613

--- java学习/shop/shop/src/main/java/com/mmall/controller/portal/UserController.java
+++ java学习/shop/shop/src/main/java/com/mmall/controller/portal/UserController.java
@@ -1,21 +1,31 @@
 package com.mmall.controller.portal;
 
 
+import com.mmall.common.Const;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.mmall.common.ServerResponse;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/user/")
 public class UserController {
+    @Autowired
+    private IUserService iUserService;
 
     @RequestMapping(value = "login",method = RequestMethod.POST)
     @ResponseBody
-    public Object login(String username, String password, HttpSession session){
-//        service -> mybatis dao
-        return null;
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> user =  iUserService.login(username,password);
+        if(user.isSuccess()){
+            session.setAttribute(Const.CURRENT_USER,user.getData());
+        }
+        return user;
     }
 }
