2af422cb148428b1450f820404df3b637da7a296
hanhongxuan <hanhongxuan@daojia.com>
2018-06-13 18:23:27 +0800

    0613

--- java学习/secKill/src/main/java/org/seckill/dto/Link.java
+++ java学习/secKill/src/main/java/org/seckill/dto/Link.java
@@ -18,7 +18,7 @@ public class Link {
     @Override
     public String toString() {
         return "Link{" +
-                " address=" + address +
+                ", address=" + address +
                 '}';
     }
 }
